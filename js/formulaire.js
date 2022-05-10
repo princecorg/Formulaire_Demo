10a59d07cd02768a5fd41083ff333d87cf2c7045
princecorg <cvallot76@gmail.com>
2022-05-10 16:37:54 +0200

    Ajout d'un visuel conditionnel sur les champs

--- js/formulaire.js
+++ js/formulaire.js
@@ -9,14 +9,26 @@ let message = document.getElementById('message')
 
 // Variables déclarées
 let messageContent
-let valide=false
+let valide = false
 
 //Tableau de mots à exclure ("gros mots")
-let motsProscrits = ["idiot","navet","pourri","arnaqueur"]
+let motsProscrits = ['idiot', 'navet', 'pourri', 'arnaqueur']
 
 //Gestion du click sur le bouton
 button.addEventListener('click', traiterFormulaire)
-
+//gestion du changement de contenu des champs (utilisation d'une ternaire dans une fonction fléchée)
+nom.addEventListener('change',() => {
+   nom.value !=""?nom.classList.remove('invalide'):nom.classList.add('invalide')
+})
+prenom.addEventListener('change',() => {
+  prenom.value !=""?prenom.classList.remove('invalide'):prenom.classList.add('invalide')
+})
+objet.addEventListener('change',() => {
+  objet.value !=""?objet.classList.remove('invalide'):objet.classList.add('invalide')
+})
+commentaire.addEventListener('change',() => {
+  commentaire.value !=""?commentaire.classList.remove('invalide'):commentaire.classList.add('invalide')
+})
 //fonction qui contrôle que les champs ne sont pas vides
 function traiterFormulaire () {
   let nomVal = nom.value
@@ -26,53 +38,63 @@ function traiterFormulaire () {
 
   if (nomVal === '') {
     messageContent = 'Le champ nom est vide'
+    nom.classList.add('invalide')
     genererMessage(messageContent)
   } else if (prenomVal === '') {
     messageContent = 'Le champ prénom est vide'
+    prenom.classList.add('invalide')
     genererMessage(messageContent)
   } else if (objetVal === '0') {
     messageContent = "Vous n'avez pas sélectionné d'objet"
+    objet.classList.add('invalide')
     genererMessage(messageContent)
   } else if (commentaireVal === '') {
     messageContent = "Vous n'avez pas saisi de message"
+    commentaire.classList.add('invalide')
     genererMessage(messageContent)
   } else {
-    if (!rgpd.checked){ 
+    if (!rgpd.checked) {
       messageContent = 'La validation du RGPD est obligatoire'
       genererMessage(messageContent)
     } else {
-        messageContent = 'Votre formulaire a été envoyé'
-    valide = true
-    genererMessage(messageContent)
+      messageContent = 'Votre formulaire a été envoyé'
+      valide = true
+      nom.classList.remove('invalide')
+      prenom.classList.remove('invalide')
+      objet.classList.remove('invalide')
+      commentaire.classList.remove('invalide')
+      genererMessage(messageContent)
     }
   }
 }
+
 // fonction qui gère l'attribution d'une classe à l'élément message
 function genererMessage (messageContent) {
-  message.textContent = messageContent  
+  message.textContent = messageContent
   if (valide) {
     message.classList.add('success')
     message.classList.remove('error')
-    message.style.display ='block'
+    message.style.display = 'block'
+    valide = false
   } else {
     message.classList.add('error')
     message.classList.remove('success')
-    message.style.display='block'
+    message.style.display = 'block'
   }
 }
 //Gestion de la saisie dans le Textarea
-commentaire.addEventListener('keyup',controlerTexte)
+commentaire.addEventListener('keyup', controlerTexte)
 
 // fonction qui contrôle la présence de mots non autorisés (les "gros mots")
-function controlerTexte() {
+function controlerTexte () {
   // boucle pour contrôler la présence de chaque mot dans la chaîne de caractères
-  for (i = 0; i<motsProscrits.length; i++) {
+  for (i = 0; i < motsProscrits.length; i++) {
     let contenuCommentaire = commentaire.value
-    if(contenuCommentaire.includes(motsProscrits[i])){
-      alert (`le mot ${motsProscrits[i]} n\'est pas autorisé ici !`)
+    if (contenuCommentaire.includes(motsProscrits[i])) {
+      alert(`le mot ${motsProscrits[i]} n\'est pas autorisé ici !`)
       //supprimer uniquement le mot proscrit du Textarea
-       motRetire = motsProscrits[i].length
-      commentaire.value = contenuCommentaire.slice(0,-motRetire)
+      motRetire = motsProscrits[i].length
+      commentaire.value = contenuCommentaire.slice(0, -motRetire)
     }
   }
 }
